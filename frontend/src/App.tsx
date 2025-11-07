d1ae75d9e29d51d717ee73983c3118b3718b24b2
Hadassa Levi <Dassy2955@gmail.com>
2025-11-07 02:11:36 +0200

    Wrap app with ToastProvider for global toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SubscriptionPage, CheckoutSuccessPage, CheckoutCancelPage } from './pages';
+import { NotificationDemo } from './pages/NotificationDemo';
+import { ToastProvider } from './contexts/ToastContext';
 
 // TEMPORARY: Showing SubscriptionPage for testing
 // TODO: Revert to original welcome screen after testing
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SubscriptionPage />} />
-        <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
-        <Route path="/checkout/cancel" element={<CheckoutCancelPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SubscriptionPage />} />
+          <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
+          <Route path="/checkout/cancel" element={<CheckoutCancelPage />} />
+          <Route path="/notification-demo" element={<NotificationDemo />} />
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   );
 }
 
